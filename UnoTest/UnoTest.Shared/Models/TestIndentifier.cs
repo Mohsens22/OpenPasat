08614c8da2d6baa53b99b1637c1a0d5cc0298444
Mohsen Seifi <mohsens22@outlook.com>
2021-01-12 18:16:53 +0330

    Added Identifier for test mode

--- UnoTest/UnoTest.Shared/Models/TestIndentifier.cs
+++ UnoTest/UnoTest.Shared/Models/TestIndentifier.cs
@@ -9,6 +9,8 @@ namespace UnoTest.Shared.Models
         public int TestCount { get; set; }
         public int Quantum { get; set; }
         public int ImpulseRate { get; set; }
+        public RepresentationType RepresentationType { get; set; }
+        public bool Correction { get; set; }
 
         public int AnswerTime { get => Quantum - ImpulseRate; }
     }
---
+++ UnoTest/UnoTest.Shared/Models/enums/RepresentationType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnoTest.Shared.Models
+{
+    public enum RepresentationType
+    {
+        UI,
+        Audio,
+        Mixed
+    }
+}
