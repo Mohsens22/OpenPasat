a567ef7a95818d25acdb92b3f0964f1a018d925e
Mohsen Seifi <mohsens2@outlook.com>
2021-01-20 16:19:15 +0330

    VLC Lib 0 zupport

---
+++ UnoTest/UnoTest.Shared/Services/VlcAudioPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnoTest.Shared.Services.Interfaces;
+
+namespace UnoTest.Shared.Services
+{
+    public class VlcAudioPlayer : IMediaPlayer
+    {
+        public void Play(Uri file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Play(int num)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
