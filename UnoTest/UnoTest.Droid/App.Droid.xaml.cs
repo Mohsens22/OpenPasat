bac8a0bab4c1407c4c3db82d89425581cdc89d22
Mohsen Seifi <mohsens2@outlook.com>
2021-02-03 02:48:12 +0330

    Build

---
+++ UnoTest/UnoTest.Droid/App.Droid.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnoTest.Shared.Infrastructure.Features;
+
+namespace UnoTest
+{
+    public partial class App
+    {
+        void RegisterPlatformServices()
+        {
+
+        }
+
+        FeatureConfiguration GetPlatformFeatures()
+        {
+            return new FeatureConfiguration
+            {
+                UiRepresentation = FeatureAvailability.Available,
+                KeyboardInput = FeatureAvailability.Available,
+                UiInput = FeatureAvailability.Available
+            };
+        }
+    }
+}
