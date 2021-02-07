f56eed749cd53242264772ca121d1d9e8b310db4
Mohsen Seifi <mohsens2@outlook.com>
2021-02-08 01:57:35 +0330

    Bitch

---
+++ UnoTest/UnoTest.Skia.Wpf/App.Skia.Wpf.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnoTest.Infrastructure.Features;
+
+namespace UnoTest
+{
+    public partial class App
+    {
+        void RegisterPlatformServices()
+        {
+
+        }
+
+        FeatureConfiguration GetPlatformFeatures()
+        {
+            return new FeatureConfiguration
+            {
+                UiRepresentation = FeatureAvailability.Available,
+                KeyboardInput = FeatureAvailability.Available,
+                UiInput = FeatureAvailability.Available
+            };
+        }
+    }
+}
