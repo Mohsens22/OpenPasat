d9ca8a21eab3f4b5f451cd0c9dcfa64aa0fa9e8e
Mohsen Seifi <mohsens2@outlook.com>
2021-02-17 13:55:47 +0330

    Implemented WASM audio

--- UnoTest/UnoTest.Wasm/App.Wasm.xaml.cs
+++ UnoTest/UnoTest.Wasm/App.Wasm.xaml.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using UnoTest.Infrastructure.Features;
+using UnoTest.Services;
+using UnoTest.Services.Generic;
+using UnoTest.Wasm.Services;
 
 namespace UnoTest
 {
@@ -11,13 +14,15 @@ namespace UnoTest
     {
         void RegisterPlatformServices()
         {
-            
+            Locator.CurrentMutable.Register(() => new AudioPlayer(), typeof(IMediaPlayer));
+            Locator.CurrentMutable.Register(() => new GenericPicker(), typeof(ISaver));
         }
 
         FeatureConfiguration GetPlatformFeatures()
         {
             return new FeatureConfiguration
             {
+                AudioRepresentation = FeatureAvailability.Available,
                 UiRepresentation = FeatureAvailability.Available,
                 KeyboardInput = FeatureAvailability.Available,
                 UiInput = FeatureAvailability.Available
