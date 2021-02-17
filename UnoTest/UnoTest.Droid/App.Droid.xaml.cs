b16b03c151821d613b06923df60765fc8cf93c2e
Mohsen Seifi <mohsens2@outlook.com>
2021-02-17 10:32:03 +0330

    File provider is implemented for Android

--- UnoTest/UnoTest.Droid/App.Droid.xaml.cs
+++ UnoTest/UnoTest.Droid/App.Droid.xaml.cs
@@ -4,11 +4,14 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Splat;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnoTest.Droid.Services;
 using UnoTest.Infrastructure.Features;
+using UnoTest.Services;
 
 namespace UnoTest
 {
@@ -16,7 +19,7 @@ namespace UnoTest
     {
         void RegisterPlatformServices()
         {
-
+            Locator.CurrentMutable.Register(() => new FilePicker(), typeof(ISaver));
         }
 
         FeatureConfiguration GetPlatformFeatures()
